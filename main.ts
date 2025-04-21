f4b7fc25f437e8c6b5f6fdd039fe8132012617e7
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-04-21 11:24:07 -0600

    chore: Installed @hono/zod-validator

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
+import { cors } from "hono/cors";
 import postsRoutes from './routes/posts.ts';
 
 const app = new Hono().basePath('/api');
 
 app.use(logger());
+app.use(cors());
 
 app.get('/', (c) => {
   c.status(418);
